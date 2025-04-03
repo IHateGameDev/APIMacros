a92f03909612a3917d9b637fb20b1a852731b1aa
_IHateGameDev <ihategamedev@proton.me>
2025-04-03 14:43:03 +0300

    Change projects structure

--- APIMacros/useful.h
+++ APIMacros/useful.h
@@ -1,12 +1,12 @@
-#ifndef __API_API_H__
-#define __API_API_H__
+#ifndef __API_USEFUL_H__
+#define __API_USEFUL_H__
 
 /*
 **  Example:
 **    typedef struct {
 **      int sP1 private;                       // Compiler show warning if use this member
 **      float sP2;                             // Compiler don't show warning
-**    } SomeStc
+**    } SomeStc;
 */
 #ifndef private
 # define private __attribute__((deprecated("This is private struct member")))
@@ -26,7 +26,7 @@
 **  Example:
 **    typedef struct {
 **      local int sP1;                         // Can't use out def file
-**    } SomeStc2
+**    } SomeStc2;
 */
 #ifndef local
 # define local __attribute__((visibility("internal")))
@@ -36,42 +36,10 @@
 **  Example:
 **    typedef struct {
 **      mlocal int sP1;                        // Can't use out module
-**    } SomeStc3
+**    } SomeStc3;
 */
 #ifndef mlocal
 # define mlocal __attribute__((visibility("hidden")))
-#endif
-
-#ifndef API
-# ifndef API_SHARED_BUILD
-#   define API_SHARED_BUILD 0
-# endif // !API_SHARED_BUILD
-
-# ifndef API_SHARED_USE
-#   define API_SHARED_USE 0
-# endif // !API_SHARED_USE
-
-# if defined(_WIN32) || defined(_WIN64)
-#   define API_SYM_EXP __declspec(dllexport)
-#   define API_SYM_IMP __declspec(dllimport)
-# else
-#   define API_SYM_EXP __attribute__((visibility("default")))
-#   define API_SYM_IMP
-# endif // _WIN*
-
-# if API_SHARED_BUILD
-#   define API_SHARED_LIB API_SYM_EXP
-# elif API_SHARED_USE
-#   define API_SHARED_LIB API_SYM_IMP
-# else
-#   define API_SHARED_LIB
-# endif // API_SHARED_*
-
-# ifdef __cplusplus
-#   define API extern "C" API_SHARED_LIB
-# else
-#   define API API_SHARED_LIB
-# endif
 #endif
 
 /*
@@ -79,8 +47,8 @@
 **    ...
 **    ON_DEBUG(
 **      if(!errID)
-**        printf("Error ID: %u", errID)
-**    )
+**        printf("Error ID: %u", errID);
+**    );
 **    ...
 */
 #ifdef DEBUG
@@ -89,4 +57,4 @@
 # define ON_DEBUG(action)
 #endif
 
-#endif // !__API_API_H__
+#endif // !__API_USEFUL_H__
