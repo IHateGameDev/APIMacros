692b2e76bb27b6528c8cf5f1e405a1187beb100b
_IHateGameDev <ihategamedev@proton.me>
2025-04-07 14:59:27 +0300

    HotFix: all.h include new files

--- APIMacros/override/all.h
+++ APIMacros/override/all.h
@@ -1,9 +1,9 @@
 #ifndef __API_CONF_H__
 #define __API_CONF_H__
 
-#include "math/conf.h"
-#include "stdint/conf.h"
-#include "stdlib/conf.h"
-#include "string/conf.h"
+#include "math.h"
+#include "stdint.h"
+#include "stdlib.h"
+#include "string.h"
 
 #endif // !__API_CONF_H__
